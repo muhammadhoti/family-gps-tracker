d0b7d2449f20affa62e6d9e645d43d6c880a2e70
Muhammad Hoti <muhammad.hoti@gmail.com>
2019-03-10 21:51:41 +0000

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -2,4 +2,4 @@ const fbAppId = '379582689533578';
 const dbRef = 'https://family-gps-tracker-13265.firebaseio.com/';
 const googleAPI = 'AIzaSyCYvMpmVhFc0ydILEuXGJNYNGFnBoKPCL8'
 export { fbAppId, dbRef, googleAPI };
-//https://expo.io/artifacts/c9e05ef1-45bd-43ca-b752-2139f795416d 
+//https://expo.io/artifacts/2d240584-9302-47b7-95ee-6e200d56b8f5
